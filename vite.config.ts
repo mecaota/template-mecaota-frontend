e233a55f79ab15dd8d26d384ab492be3bba12b85
mecaota <24618982+mecaota@users.noreply.github.com>
2021-07-26 20:38:22 +0900

    paths settings from vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  resolve: {
-    alias: {
-      '@assets': path.join(__dirname, 'src/assets'),
-      '@atoms': path.join(__dirname, 'src/components/atoms'),
-      '@molecules': path.join(__dirname, 'src/components/molecules'),
-      '@lib': path.join(__dirname, 'src/lib'),
-      '@organisms': path.join(__dirname, 'src/components/organisms'),
-    },
-  },
+  plugins: [reactRefresh(), tsconfigPaths()],
 });
