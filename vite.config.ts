282be24a9dd9d37f59b42f94c2c0069617145317
mecaota <24618982+mecaota@users.noreply.github.com>
2022-05-11 15:45:53 +0900

    eslint-import-pluginを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import VitePluginLinaria from 'vite-plugin-linaria';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths(), VitePluginLinaria()],
+  plugins: [react(), tsconfigPaths(), VitePluginLinaria()],
 });
