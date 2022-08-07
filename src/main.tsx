267e21e119ab3e22a18ef169f2350840cc8c255a
mecaota <24618982+mecaota@users.noreply.github.com>
2022-08-07 23:26:50 +0900

    prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import { Router } from './routes';
 
 const idElement = document.getElementById('root');
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,6 @@
+import { BananaButton } from '@molecules/bananaButton';
 import { memo } from 'react';
 import type { FC } from 'react';
-import { BananaButton } from '@molecules/bananaButton';
 
 export const IndexPage: FC = memo(() => {
   return (
